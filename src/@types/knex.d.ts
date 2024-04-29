6c8dfa90932b4ba158bb51b349d3f3ebfa9cef3c
Gabriel Gamboa <gabriel_gamboa10@hotmail.com>
2024-04-29 14:54:38 -0300

    feat: create meal creation

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,13 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Knex } from "knex";
 
+interface Users {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+interface Meals {
+  id: string;
+  user_id: string;
+  name: string;
+  description: string;
+  date: Date;
+  is_on_diet: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
 declare module "knex/types/tables" {
   export interface Tables {
-    users: {
-      id: string;
-      name: string;
-      email: string;
-      created_at: string;
-    };
+    users: Users;
+    meals: Meals;
   }
 }
--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { checkIfUserIdExists } from "../middlewares/check-if-userId-exists";
+import { z } from "zod";
+import { knex } from "../database/config";
+import { randomUUID } from "crypto";
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post(
@@ -7,6 +10,32 @@ export async function mealsRoutes(app: FastifyInstance) {
     {
       preHandler: [checkIfUserIdExists],
     },
-    async (request, reply) => {},
+    async (request, reply) => {
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(), // todo: validate hour too
+      });
+
+      const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+        request.body,
+      );
+
+      console.log({ name, description, isOnDiet, date });
+
+      const userId = request.user?.id;
+
+      await knex("meals").insert({
+        id: randomUUID(),
+        name,
+        description,
+        user_id: userId,
+        date, // ver como tá salvando o date
+        is_on_diet: isOnDiet,
+      });
+
+      return reply.status(201).send();
+    },
   );
 }
